1551fa90c0e98da84125f446602acc62ad3adde6
hariharran <hariharran@gmail.com>
2023-09-22 17:59:39 +0530

    commit first

---
+++ routes/hotels.js
@@ -0,0 +1,24 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from"../utils/verifyToken.js"
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+} from "../controllers/hotel.js";
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+//UPDATE
+router.put("/:id",verifyAdmin,updateHotel);
+//DELETE
+router.delete("/:id",verifyAdmin, deleteHotel);
+//GET
+router.get("/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
+export default router;
